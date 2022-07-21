7f587693a688c1d16ada8e7a3cd5fd1aa0ac9576
Ayilk <srangel.h92@gmail.com>
2022-07-21 12:16:49 -0500

    Colocando tipado a las peticiones http

--- src/app/gifts/services/gifts.service.ts
+++ src/app/gifts/services/gifts.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gift, SearchGiftsResponse } from '../interfaces/gifts.interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class GiftsService {
   private apiKey: string = 'mzq5EH8zMZsmSuyDIdGkKHgD8ixWKb77'
   private _historial: string[] = [];
  //cambiar any por su tipo correspondiente
-  public resultados: any[] = [];
+  public resultados: Gift[] = [];
 
   get historial(){
     // this._historial = this._historial.splice(0,10)
@@ -26,10 +27,11 @@ export class GiftsService {
       this._historial = this._historial.splice(0,10)
     }
     console.log(this._historial)
-    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=mzq5EH8zMZsmSuyDIdGkKHgD8ixWKb77&q=${query}&limit=5`)
-    .subscribe((respuesta:any) => {
+    this.http.get<SearchGiftsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=mzq5EH8zMZsmSuyDIdGkKHgD8ixWKb77&q=${query}&limit=5`)
+    .subscribe((respuesta) => {
       console.log(respuesta.data)
-      this.resultados = respuesta.data
+      this.resultados = respuesta.data;
+      
     })
   }
 }
